b44c4d429f6b834ce7cd9a8ce2f56a596ff4e2cf
yuta-o-note <yuta.o.note@gmail.com>
2021-02-12 09:39:20 +0900

    feat: Create Holiday Alert with Google Calendar API

---
+++ app/Calendar.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+
+class Calendar
+{
+    // Googleの提供する日本の祝日カレンダー
+    private $calendarId = 'japanese__ja@holiday.calendar.google.com';
+    private $googleMapApiKey;
+
+    public function __construct(string $googleMapApiKey)
+    {
+        $this->googleMapApiKey = $googleMapApiKey;
+    }
+
+    /**
+     * 指定した期間の祝日を取得する
+     *
+     * @param Carbon $start
+     * @param Carbon $end
+     * @return array
+     */
+    public function holidays(Carbon $start, Carbon $end)
+    {
+        $calendar_id = urlencode($this->calendarId);
+
+        $url = "https://www.googleapis.com/calendar/v3/calendars/" . $calendar_id . "/events?";
+        $query = [
+            'key' => $this->googleMapApiKey,
+            'timeMin' => $start->format('Y-m-d\TH:i:s\Z'),
+            'timeMax' => $end->format('Y-m-d\TH:i:s\Z'),
+            'maxResults' => 50,
+            'orderBy' => 'startTime',
+            'singleEvents' => 'true'
+        ];
+
+        $holidays = [];
+        if ($data = file_get_contents($url . http_build_query($query), true)) {
+            $data = json_decode($data);
+            foreach ($data->items as $row) {
+                $holiday = Carbon::parse($row->start->date);
+                $holidays[$holiday->format('Y-m-d')] = [
+                    'name' => $row->summary,
+                    'date' => $holiday,
+                ];
+            }
+        }
+
+        return $holidays;
+    }
+
+    /**
+     * 指定した日付が祝日であれば概要を返す
+     *
+     * @param Carbon $target
+     * @return array|null
+     */
+    public function checkHoliday(Carbon $target)
+    {
+        $start = Carbon::parse($target->format("Y-m-d"));
+        $end = $start->copy()->addDay();
+        $holidays = $this->holidays($start, $end);
+        if (count($holidays)) {
+            return array_shift($holidays);
+        }
+        return null;
+    }
+}
--- index.php
+++ index.php
@@ -2,5 +2,20 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$slack = new App\Slack($_ENV['OAUTH_ACCESS_TOKEN']);
-$slack->alert('#general', 'botとして投稿');
+use App\Calendar;
+use App\Slack;
+use Carbon\Carbon;
+
+// 何日前に通知するか
+$notifyBeforeDays = 7;
+date_default_timezone_set('Asia/Tokyo');
+
+$calender = new Calendar($_ENV['GOOGLE_MAP_API_KEY']);
+$target = Carbon::today()->addDays($notifyBeforeDays);
+$holiday = $calender->checkHoliday($target);
+
+if ($holiday) {
+    $message = $holiday['date']->format('Y年m月d日') . "は" . $holiday['name'] . "です";
+    $slack = new Slack($_ENV['OAUTH_ACCESS_TOKEN']);
+    $slack->alert('#general', $message);
+}
